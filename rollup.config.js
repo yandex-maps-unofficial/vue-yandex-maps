d686697806009505067ea1f01ca530795ea507b8
Vlad Tokarev <Vadiok@users.noreply.github.com>
2022-09-12 12:14:49 +0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,21 @@
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
 
-export default {
+const getBuildSectionConfig = (format, file) => ({
   input: 'src/index.ts',
   output: {
-    dir: 'dist',
-    format: 'esm',
+    name: 'vue-yandex-maps',
+    file,
+    format,
+    exports: 'named',
     sourcemap: true,
   },
   plugins: [typescript({ tsconfig: './tsconfig.json' }), terser()],
   external: ['vue'],
-};
+});
+
+export default [
+  getBuildSectionConfig('esm', pkg.main),
+  getBuildSectionConfig('cjs', pkg.module),
+];
