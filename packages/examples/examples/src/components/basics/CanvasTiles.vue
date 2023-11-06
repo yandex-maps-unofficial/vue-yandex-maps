b5937765b391171fc5bb4bb60879eb4922253122
daniluk4000 <daniluk4000@outlook.com>
2023-11-06 16:11:40 +0300

    Fix canvas tiles example

--- packages/examples/examples/src/components/basics/CanvasTiles.vue
+++ packages/examples/examples/src/components/basics/CanvasTiles.vue
@@ -17,11 +17,14 @@
         :height="height"
       >
         <yandex-map-default-scheme-layer :settings="{ theme }" />
-        <yandex-map-tile-data-source :settings="{ id: layerId, raster: dataSource }" />
-        <yandex-map-layer :settings="{ id: layerId, source: layerId, type: layerId }" />
         <yandex-map-controls :settings="{ position: 'right' }">
           <yandex-map-zoom-control />
         </yandex-map-controls>
+
+        <template v-if="dataSource">
+          <yandex-map-tile-data-source :settings="{ id: layerId, raster: dataSource }" />
+          <yandex-map-layer :settings="{ id: layerId, source: layerId, type: layerId }" />
+        </template>
       </yandex-map>
       <!-- #endregion html -->
     </template>
@@ -40,46 +43,46 @@ import {
   YandexMapZoomControl,
 } from 'vue-yandex-maps';
 import { onMounted } from 'vue';
+import type { YMapTileDataSourceProps } from '@yandex/ymaps3-types';
 
 const TILE_SIZE = 256;
 
 const layerId = 'A';
 
-let image: HTMLImageElement | null;
+let dataSource: YMapTileDataSourceProps['raster'] | undefined;
 
 onMounted(() => {
-  image = new Image();
-});
+  const image = new Image();
 
-const load = new Promise((resolve) => {
-  if (!image) return;
-  image.src = '/vue-yandex-maps/ship.svg';
-  if (image.complete) {
-    resolve(image);
-  } else {
-    image.onload = () => resolve(image);
-    image.onerror = () => resolve(image);
-  }
-});
+  const load = new Promise((resolve) => {
+    image.src = '/vue-yandex-maps/ship.svg';
+    if (image.complete) {
+      resolve(image);
+    } else {
+      image.onload = () => resolve(image);
+      image.onerror = () => resolve(image);
+    }
+  });
 
-const dataSource = {
-  type: layerId,
-  fetchTile: async (x: number, y: number, z: number) => {
-    const canvas = document.createElement('canvas');
-    canvas.width = TILE_SIZE;
-    canvas.height = TILE_SIZE;
-    const ctx = canvas.getContext('2d')!;
-    ctx.strokeStyle = 'red';
-    ctx.strokeRect(0, 0, TILE_SIZE, TILE_SIZE);
-    ctx.fillText(JSON.stringify({
-      x,
-      y,
-      z,
-    }), 10, 10);
-    ctx.drawImage(image!, 0, 0);
-    await load;
-    return ({ image: canvas });
-  },
-};
+  dataSource = {
+    type: layerId,
+    fetchTile: async (x: number, y: number, z: number) => {
+      const canvas = document.createElement('canvas');
+      canvas.width = TILE_SIZE;
+      canvas.height = TILE_SIZE;
+      const ctx = canvas.getContext('2d')!;
+      ctx.strokeStyle = 'red';
+      ctx.strokeRect(0, 0, TILE_SIZE, TILE_SIZE);
+      ctx.fillText(JSON.stringify({
+        x,
+        y,
+        z,
+      }), 10, 10);
+      ctx.drawImage(image, 0, 0);
+      await load;
+      return ({ image: canvas });
+    },
+  };
+});
 // #endregion setup
 </script>
