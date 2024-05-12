42473d3658215b289ada31907168de8e4d067ef5
Pavel Galkin <bizz.gc@gmail.com>
2024-05-12 13:31:48 +0300

    Docs update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -35,7 +35,7 @@ export default defineUserConfig({
         selectLanguageText: 'Язык',
         selectLanguageName: 'Русский',
         navbar: [
-          { text: 'v1.x-beta', children: [{ text: 'v0.x', link: 'https://vue-yandex-maps.github.io/' }] },
+          { text: 'v1.x', children: [{ text: 'v2.x', link: 'https://yandex-maps-unofficial.github.io/vue-yandex-maps/' }] },
           { text: 'Руководство', link: '/guide/' },
           { text: 'Примеры', link: '/examples/map-create' },
           { text: 'API Я.Карт', link: 'https://tech.yandex.ru/maps/doc/jsapi/2.1/quick-start/index-docpage/' },
@@ -49,7 +49,7 @@ export default defineUserConfig({
         selectLanguageText: 'Language',
         selectLanguageName: 'English',
         navbar: [
-          { text: 'v1.x-beta', children: [{ text: 'v0.x', link: 'https://vue-yandex-maps.github.io/' }] },
+          { text: 'v1.x', children: [{ text: 'v2.x', link: 'https://yandex-maps-unofficial.github.io/vue-yandex-maps/' }] },
           { text: 'Guide', link: '/en/guide/' },
           { text: 'Examples', link: '/en/examples/' },
           { text: 'YandexMap API ', link: 'https://tech.yandex.ru/maps/doc/jsapi/2.1/quick-start/index-docpage/' },
