520e4c4b974260bb61621ecf6636259fc431a9aa
daniluk4000 <daniluk4000@outlook.com>
2024-07-15 13:15:16 +0300

    Closes #253

--- packages/vue-yandex-maps/src/components/YandexMap.vue
+++ packages/vue-yandex-maps/src/components/YandexMap.vue
@@ -249,26 +249,6 @@ export default defineComponent({
                 else if (listener) map.value.removeChild(listener);
             }, { immediate: true });
 
-            const eventListener = (event: MouseEvent) => {
-                const target = event.target as HTMLDivElement;
-
-                // Just in case something else is triggered
-                if (!target || !('classList' in target)) return;
-
-                // Closest can be undefined in IE. Not sure if anybody uses it but just in case
-                if (target.classList.contains('ymaps3x0--button') || target.closest?.('.ymaps3x0--button')) event.preventDefault();
-            };
-
-            watch(ymapContainer, container => {
-                if (!container) return;
-
-                // If it suddenly had that and Vue double-triggered ref
-                container.removeEventListener('click', eventListener, { capture: true });
-                container.addEventListener('click', eventListener, { capture: true });
-            }, {
-                immediate: true,
-            });
-
             await setFragment();
 
             if (!VueYandexMaps.isLoaded.value) {
