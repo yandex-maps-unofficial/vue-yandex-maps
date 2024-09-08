402e81f03de8de686710ebe836efdf8e96099068
daniluk4000 <daniluk4000@outlook.com>
2024-09-08 16:11:32 +0300

    fix: docs

--- packages/examples/examples/src/components/basics/MapBasics.vue
+++ packages/examples/examples/src/components/basics/MapBasics.vue
@@ -19,7 +19,6 @@
                 :width="width"
             >
                 <yandex-map-default-scheme-layer/>
-                <yandex-map-carparks-layer/>
             </yandex-map>
             <!-- #endregion html -->
         </template>
@@ -29,6 +28,6 @@
 <script setup lang="ts">
 import CommonWrapper from '../CommonWrapper.vue';
 // #region setup
-import { YandexMap, YandexMapDefaultSchemeLayer, YandexMapCarparksLayer } from 'vue-yandex-maps';
+import { YandexMap, YandexMapDefaultSchemeLayer } from 'vue-yandex-maps';
 // #endregion setup
 </script>
